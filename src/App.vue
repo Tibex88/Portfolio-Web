ed3918269eb8fc6fd6f1d3a136f54ebee266b3eb
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2022-10-16 01:30:35 +0300

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
+  <div id="menu-btn" class="fas fa-bars"></div>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <base-button></base-button>
   <base-card></base-card>
   <base-input></base-input>
+
+  <div id="theme-toggler" class="fas fa-moon"></div>
 </template>
 
 <script>
@@ -22,6 +25,42 @@ export default {
 </script>
 
 <style>
+
+:root{
+    --green:#27ae60;
+    --black:#444;
+    --light-color:#777;
+    --bg-color:#e6e6e6;
+    --border:.1rem solid rgba(0,0,0,.2);
+    --box-shadow:.4rem .4rem 1rem #ccc,
+                -.4rem -.4rem 1rem #fff;
+    --box-shadow-inset:.4rem .4rem 1rem #ccc inset,
+                      -.4rem -.4rem 1rem #fff inset;
+}
+
+*{
+    font-family: 'Poppins', sans-serif;
+    margin:0; padding:0;
+    box-sizing: border-box;
+    outline: none; border:none;
+    text-decoration: none;
+    text-transform: capitalize;
+    transition: .2s linear;
+}
+
+html{
+    font-size: 62.5%;
+    overflow-x: hidden;
+    scroll-behavior: smooth;
+}
+
+body{
+    background: var(--bg-color);
+    padding:0 12rem 0 12rem;
+    padding-left: 30rem;
+    margin:(100%-24px)
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,5 +68,48 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#theme-toggler{
+    position: fixed;
+    top:1.5rem; right: 2rem;
+    z-index: 1000;
+    height: 5rem;
+    width: 5rem;
+    line-height: 5rem;
+    border-radius: 50%;
+    font-size: 2rem;
+    cursor: pointer;
+    box-shadow: var(--box-shadow);
+    text-align: center;
+    color:var(--black);
+    background: var(--bg-color);
+}
+
+#theme-toggler:hover{
+    color:var(--green);
+    box-shadow: var(--box-shadow-inset);
+}
+
+#menu-btn{
+    position: fixed;
+    top:1.5rem; left: 2rem;
+    z-index: 1000;
+    height: 5rem;
+    width: 5rem;
+    line-height: 5rem;
+    border-radius: 50%;
+    font-size: 2rem;
+    cursor: pointer;
+    box-shadow: var(--box-shadow);
+    text-align: center;
+    color:var(--black);
+    background: var(--bg-color);
+    /* display: none; */
+}
+
+#menu-btn:hover{
+    color:var(--green);
+    box-shadow: var(--box-shadow-inset);
 }
 </style>
---
+++ src/components/contact/ContactSection.vue
@@ -0,0 +1,23 @@
+<template>
+<base-input></base-input>
+<base-card></base-card>
+<base-button></base-button>
+</template>
+
+<script>
+import BaseInput from '../UI/BaseInput.vue'
+import BaseCard from '../UI/BaseCard.vue'
+import BaseButton from '../UI/BaseButton.vue'
+
+export default{
+    components:{
+        BaseInput,
+        BaseCard,
+        BaseButton,
+    }
+}
+</script>
+
+<style>
+
+</style>
